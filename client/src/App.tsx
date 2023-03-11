abfdd98d842a95e514845a287b8b514797cc8021
Filip <filip.knias@o2.pl>
2023-03-11 16:59:23 +0100

    delete LinksList component and code refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Home } from './views';
 
 export const App = () => {
   return (
-    <div className="bg-gray-100 h-full">
+    <div className="bg-gray-100 h-screen">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,19 +1,18 @@
-import { Brand } from "../components/common";
+import { Brand, Button } from "../components/common";
 import { CreateLink, ShareLink, QrCode } from '../components/core';
-import { LinksList } from "../components/core/LinksList";
 
 export const Home = () => {
   return (
-    <div className="container mx-auto xl:w-1/3 py-8">
-      <div className="bg-white rounded-md py-6 px-4 mx-auto shadow-sm border border-gray-200">
-        <div className="mb-8">
+    <div className="container mx-auto xl:w-1/3 h-full py-8 px-4">
+      <div className="bg-white rounded-md py-6 px-4 mx-auto shadow-sm border border-gray-200 min-h-full">
+        <div className="mb-8 flex justify-between items-center">
           <Brand />
+          <Button>Dashboard</Button>
         </div>
         <div className="flex flex-col gap-8 flex-1">
           <CreateLink />
-          <ShareLink />
-          <QrCode />
-          <LinksList />
+          {/* <ShareLink />
+          <QrCode /> */}
         </div>
       </div>
     </div>
